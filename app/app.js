4dcb120095aa0de8611bb24150a7716db542cbf2
Denisse03 <99741771+Denisse03@users.noreply.github.com>
2022-09-25 21:15:44 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,25 @@
+import * as MODEL from "./model.js";
+
+function route() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  console.log("hash " + pageID);
+
+  if (pageID == "") {
+    MODEL.changePage("home");
+  } else {
+    MODEL.changePage(pageID);
+  }
+}
+
+function initListeners() {}
+
+function initApp() {
+  $(window).on("hashchange", route);
+  route();
+}
+
+$(document).ready(function () {
+  initApp();
+  initListeners();
+});
---
+++ app/model.js
@@ -0,0 +1,256 @@
+var homeContent = `<div class="banner">
+<div class="para">HOME</div>
+</div>
+
+<div class="store">
+<div class="stores">
+  <div class="outside"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+  <div class="outside"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+</div>
+
+<div class="stores">
+  <div class="inside"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+
+  <div class="inside"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+</div>
+
+<div class="stores">
+  <div class="people"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+
+  <div class="people"></div>
+  <div class="out">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure
+    voluptatibus voluptate nisi, rem cumque natus fugiat cupiditate
+    ullam at non totam ipsum quos? Consequuntur sit qui magnam
+    perferendis atque.
+  </div>
+</div>
+</div>`;
+
+var aboutContent = `<div class="logo">
+<div class="para2">ABOUT</div>
+</div>
+
+<div class="workers">
+<div class="worker1"></div>
+
+<div class="worker2"></div>
+
+<div class="worker3"></div>
+</div>
+
+<div class="texts">
+<div class="text1">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque minus
+  quo animi atque ex neque, consectetur porro beatae excepturi culpa eum
+  accusantium? Accusantium molestiae cumque perspiciatis aliquid vel non
+  modi!
+</div>
+
+<div class="text2">
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+  quaerat repellendus ut magni tempore. Vitae, voluptatibus quaerat
+  voluptatum quasi labore eum iste laborum, eveniet maiores itaque magni
+  dolor adipisci quos!
+</div>
+
+<div class="text3">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, id.
+  Impedit ea dignissimos, quasi ab iste quam sit explicabo libero
+  adipisci nihil? Alias vel dolore consequuntur cum ducimus consectetur
+  tempore?
+</div>
+</div>
+
+<div class="workers">
+<div class="worker1"></div>
+
+<div class="worker2"></div>
+
+<div class="worker3"></div>
+</div>
+
+<div class="texts">
+<div class="text1">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque minus
+  quo animi atque ex neque, consectetur porro beatae excepturi culpa eum
+  accusantium? Accusantium molestiae cumque perspiciatis aliquid vel non
+  modi!
+</div>
+
+<div class="text2">
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+  quaerat repellendus ut magni tempore. Vitae, voluptatibus quaerat
+  voluptatum quasi labore eum iste laborum, eveniet maiores itaque magni
+  dolor adipisci quos!
+</div>
+
+<div class="text3">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, id.
+  Impedit ea dignissimos, quasi ab iste quam sit explicabo libero
+  adipisci nihil? Alias vel dolore consequuntur cum ducimus consectetur
+  tempore?
+</div>
+</div>`;
+
+var foodContent = `<div class="sign">
+<div class="para3">FOODS</div>
+</div>
+
+<div class="foods">
+<div class="eat">
+  <div class="food1"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+
+  <div class="food1"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+
+<div class="eat">
+  <div class="food2"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+  <div class="food2"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+
+<div class="eat">
+  <div class="food3"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+  <div class="food3"></div>
+  <div class="food-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+</div>`;
+
+var drinksContent = `<div class="big">
+<div class="para4">BEVERAGES</div>
+</div>
+
+<div class="beverages">
+<div class="drink">
+  <div class="drink1"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+  <div class="drink1"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+
+<div class="drink">
+  <div class="drink2"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+  <div class="drink2"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+
+<div class="drink">
+  <div class="drink3"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+  <div class="drink3"></div>
+  <div class="drink-text1">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+    doloribus ratione natus, aspernatur recusandae perferendis quod ex
+    corporis facilis iste rem maxime magnam ipsa ullam reiciendis
+    veritatis ea adipisci ipsam?
+  </div>
+</div>
+</div>`;
+
+var directionContent = `<div class="large">
+<div class="para5">DIRECTION</div>
+</div>
+
+<div class="map"></div>
+
+<div class="address">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae et
+dolor soluta aperiam molestias sed nostrum quam sint eum amet, nisi
+error beatae nemo incidunt doloribus recusandae aut ipsa excepturi.
+</div>`;
+
+export function changePage(pageId) {
+  let page = pageId + "Content";
+  $("#app").html(eval(page));
+}
